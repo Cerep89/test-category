a8d0753521a37c71de505666621631cd68aed527
michael <michael.karabetsky@gmail.com>
2018-01-09 21:00:54 +0200

    Now you can change language

--- index.php
+++ index.php
@@ -3,26 +3,24 @@ require_once "language_class.php";
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
-function getLanguage()
-{
-    preg_match_all('/([a-z]{1,8}(?:-[a-z]{1,8})?)(?:;q=([0-9.]+))?/', strtolower($_SERVER["HTTP_ACCEPT_LANGUAGE"]), $matches); // Получаем массив $matches с соответствиями
-    $langs = array_combine($matches[1], $matches[2]); // Создаём массив с ключами $matches[1] и значениями $matches[2]
-    foreach ($langs as $n => $v)
-        $langs[$n] = $v ? $v : 1; // Если нет q, то ставим значение 1
-    arsort($langs); // Сортируем по убыванию q
-    $default_lang = key($langs); // Берём 1-й ключ первого (текущего) элемента (он же максимальный по q)
-    if(strpos($default_lang, "ro") !== false) return "ro";
-    elseif (strpos($default_lang, "en") !== false) return "en";
-    return $default_lang; // Выводим язык по умолчанию
+$sites = ["ro" => "Romana",
+    "en" => "English",
+];
+if (isset($_GET['lang'])) {
+    $language = $_GET['lang'];
+} else {
+    $language = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 1);
+    if (!in_array($language, array_keys($sites))) {
+        $language = 'ro';
+    }
 }
-$language = getLanguage();
-$language = "en";
 $mysqli = new mysqli("127.0.0.1", "root", "mysql", "test-category");
 $mysqli->set_charset("utf8");
 $result_set = $mysqli->query("SELECT `title_$language` FROM `category`");
 $categories = $result_set->fetch_all();
 $mysqli->close();
 $lang = new Language($language);
+
 ?>
 <!doctype html>
 <html lang="en">
@@ -31,11 +29,13 @@ $lang = new Language($language);
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?=$lang->get("TITLE_MAIN")?></title>
+    <title><?= $lang->get("TITLE_MAIN") ?></title>
 </head>
 <body>
-<?php foreach ($categories as $category):?>
-<h2><?=$category[0];?></h2>
-<?php endforeach;?>
+<a href="?lang=ro">RO</a>|<a href="?lang=en">EN</a>
+<h2><?=$lang->get("CATEGORIES")?> </h2>
+<?php foreach ($categories as $category): ?>
+    <h3><?= $category[0]; ?></h3>
+<?php endforeach; ?>
 </body>
 </html>
